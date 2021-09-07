6878d89e1f3cf8db78b15556383fd3f09eeefd5b
zhan <jujaezen@gmail.com>
2021-09-07 09:53:22 +0200

    added pimeta dependency

--- R/noma.R
+++ R/noma.R
@@ -1,8 +1,7 @@
 noma <- function(Yk, Sk, dfk, alpha) {
-  # TODO: Implement tau2h and cima
-  tau2h <- tau2h(Yk, Sk, method="ML")$tau2h
+  tau2h <- pimeta::tau2h(Yk, Sk, method="ML")$tau2h
   w <- (Sk^2 + tau2h)^-1
   theta_hat <- sum(y*w)/sum(w)
-  fit <- cima(Yk, Sk, alpha=alpha, method="BC")
+  fit <- pimeta::cima(Yk, Sk, alpha=alpha, method="BC")
   return(list(theta=theta_hat, tau2=tau2hat, ci=c(fit$lci, fit$uci)))
 }
